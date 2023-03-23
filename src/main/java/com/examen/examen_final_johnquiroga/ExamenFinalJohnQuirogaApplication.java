ca0fcc85b7f40d0233129a4f51aba02b0a3810a1
DannyJ2001 <100298882+Johnquiroga12@users.noreply.github.com>
2023-03-23 04:40:01 -0500

    si se pudo

---
+++ src/main/java/com/examen/examen_final_johnquiroga/ExamenFinalJohnQuirogaApplication.java
@@ -0,0 +1,13 @@
+package com.examen.examen_final_johnquiroga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalJohnQuirogaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenFinalJohnQuirogaApplication.class, args);
+    }
+
+}
